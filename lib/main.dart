4a4d6f11d252551edbe16758e1e2f045e9b9173b
NouranAlSharawneh <noransharawna@hotmail.com>
2024-02-12 16:44:16 +0300

    #

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+      useMaterial3: true,
         textTheme: const TextTheme(
           titleLarge: TextStyle(
             fontFamily: 'Poppins',
